adf6fa1b6e4ba49ee70b8432c5552bc48d1dc09a
ozidom <dommillar@gmail.com>
2024-09-22 21:06:19 +1000

    changed help text

--- textcontent.js
+++ textcontent.js
@@ -1,7 +1,7 @@
-const HELP_TEXT = "<h2>Help</h2><p>This is where you can provide help information on how to play the game, rules, controls, etc.</p>";
+const HELP_TEXT = "<h2>Help</h2><p>All you need to do is move a chess piece from the lower left hand corner to the lower right.</p><p>♚ Kings move one space any direction, ♜ Rooks (look like castles) move in cardinal directions (N,S,E and W),♝Bishops move diagonal and ♞ Kinghts move in a L shape but they can jump spaces. A knight moves in an  L shape: two squares in one direction, then one square sideways, and can jump over other pieces on the board.</p>";
 
 
-const CONTACT_TEXT = "<h2>Contact Us</h2><p>For any inquiries, please email us at support@mazechess.com.</p>";
+const WHI_TEXT = "<h2>Contact Us</h2><p>For any inquiries, please email us at bardofthedungeon.com.</p>";
 
 
 const TRAINING_TEXT = "<h2>Training</h2><p>This section will offer training tips, tutorials, and practice games for beginners.</p>";
