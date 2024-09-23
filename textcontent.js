4092af15e83cfdbbe814c0e5e0fb8a0609797706
ozidom <dommillar@gmail.com>
2024-09-24 07:33:29 +1000

    UI Improvements around the training grounds

--- textcontent.js
+++ textcontent.js
@@ -1,4 +1,4 @@
-const GAME_TEXT = "Move a single piece (King,Bishop,Rook or Knight) from the botton left to bottom right of the chessboard. You can switch at any time for the cost of a move";
+const GAME_TEXT = "Move a piece (King,Bishop,Rook or Knight) from the botton left to top right of the chessboard. You can switch at any time.";
 const HELP_TEXT = "<h2>Help</h2><p>All you need to do is move a chess piece from the lower left hand corner to the lower right.</p><p>♚ Kings move one space any direction, ♜ Rooks (look like castles) move in cardinal directions (N,S,E and W),♝Bishops move diagonal and ♞ Kinghts move in a L shape but they can jump spaces. A knight moves in an  L shape: two squares in one direction, then one square sideways, and can jump over other pieces on the board.</p>";
 const WHY_TEXT = "<h2>Contact Us</h2><p>For any inquiries, please email us at bardofthedungeon.com.</p>";
 const HELP_US_TEXT = "<h2>HELP</h2><p>We need your help to get the word out about Mazechess. Consider anything:<UL>Blak<LI></LI>.</p>";
